cd8958c8facc4a222dae52075aa6572a257131c7
allanitis <matthewa021@gmail.com>
2019-10-17 17:40:21 +0100

    Add authentication

---
+++ TransferWise-POC/currency_cloud_authentication.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def handler(event, context):
+    id = "matt@fintechsandpit.com"
+    user = "e48a2102f918bab738e327af297f5f335231ffb0803d4d513a6b7a808f0d2c19"
+
+    print(event)
+    
+    try:
+        print("trying to request")
+        res = requests.post(
+            "https://devapi.currencycloud.com/v2/authenticate/api",
+            params={},
+            headers={"Accept":"application/json","Content-Type":"application/x-www-form-urlencoded"},
+            data="login_id={}&api_key={}".format(id, user)
+        )
+        auth_token = json.loads(res.text)["auth_token"]
+    except BaseException as e:
+        print(e)
+        print('exception found')
+        raise(e)
+    
+    return {"message": "Successfully executed", "auth_token": auth_token}

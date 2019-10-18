5457d2b90bb548f3e56b63a00acbbe39cbfc14ab
allanitis <matthewa021@gmail.com>
2019-10-18 07:02:43 +0100

    at

--- TransferWise-POC/currency_cloud_conversion.py
+++ TransferWise-POC/currency_cloud_conversion.py
@@ -1,7 +1,7 @@
-from cc_auth import Auth
+from cc_auth_1 import Auth
 
 def handler(event, context):
-    a = Auth.get_auth_token(event)
+    a = Auth.get_auth_token()
     print(a)
 
     return {"message": "Successfully executed"}

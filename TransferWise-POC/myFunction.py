f9f4a54bd151be563cef9438d4760a83b8f7173c
allanitis <matthewa021@gmail.com>
2020-07-31 15:12:30 +0100

    My changes

--- TransferWise-POC/myFunction.py
+++ TransferWise-POC/myFunction.py
@@ -1,3 +1,4 @@
+from botocore.vendored import requests
 import boto3
 import dash
 
@@ -6,6 +7,16 @@ try:
     data = ddb.get_item(
         TableName='myNewDataTable',
         Key={'name':{'S':'matt'}}
+    try:
+        res = requests.get(
+            "https://api.sandbox.transferwise.tech/v1/rates",
+            params={"source":"usd"},
+            headers={"Accept":"application/json"}
+        )
+        # your code goes here
+    except BaseException as e:
+        # error handling goes here
+        raise(e)
     )
 except BaseException as e:
     print(e)
